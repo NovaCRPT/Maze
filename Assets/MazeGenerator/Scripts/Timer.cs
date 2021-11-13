6d4d933d117a0cc91bd7866069d57f1d08818bad
ION Nova <ioncrpt@gmail.com>
2021-11-13 22:38:28 +0100

    Initial Upload

---
+++ Assets/MazeGenerator/Scripts/Timer.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    public TextMeshProUGUI GameOverText;
+    public int maxSeconds;
+    private float startTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {   
+        GameOverText.enabled=false;
+        startTime = Time.time;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+			RestartGame();
+		}
+
+		float t = Time.time - startTime;
+        string minutes = ((int) t /60).ToString();
+        int seconds =  (int)(t % 60);
+        int secondsElapsed = maxSeconds - seconds;
+        if(secondsElapsed>=0)
+        TimerText.text = secondsElapsed.ToString();
+        else{ 
+        this.GameOver();
+        }
+        
+    }
+
+	private static void RestartGame() {
+		Scene scene = SceneManager.GetActiveScene();
+		SceneManager.LoadScene(scene.name);
+	}
+
+	// Update is called once per frame
+	void GameOver()
+    {
+      GameOverText.enabled=true; 
+       TimerText.enabled=false; 
+        PauseGame();
+    }
+
+    void PauseGame ()
+    {
+        Time.timeScale = 0;
+    }
+
+void ResumeGame ()
+    {
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/MazePlayer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazePlayer : MonoBehaviour
+{
+	public AudioClip JumpSound = null;
+	public AudioClip HitSound = null;
+	public AudioClip CoinSound = null;
+
+	private Rigidbody mRigidBody = null;
+	private AudioSource mAudioSource = null;
+	private bool mFloorTouched = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag.Equals ("Floor")) {
+			mFloorTouched = true;
+			if (mAudioSource != null && HitSound != null && coll.relativeVelocity.y > .5f) {
+				mAudioSource.PlayOneShot (HitSound, coll.relativeVelocity.magnitude);
+			}
+		} else {
+			if (mAudioSource != null && HitSound != null && coll.relativeVelocity.magnitude > 2f) {
+				mAudioSource.PlayOneShot (HitSound, coll.relativeVelocity.magnitude);
+			}
+		}
+	}
+
+	void OnCollisionExit(Collision coll){
+		if (coll.gameObject.tag.Equals ("Floor")) {
+			mFloorTouched = false;
+		}
+	}
+
+	[System.Obsolete]
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("Coin")) {
+			if(mAudioSource != null && CoinSound != null){
+				mAudioSource.PlayOneShot(CoinSound);
+			}
+			Destroy(other.gameObject);
+			
+	Application.ExternalEval("window.open(\"https://www.youtube.com/watch?v=1nCqRmx3Dnw\")");
+		}
+	}
+
+	
+}
